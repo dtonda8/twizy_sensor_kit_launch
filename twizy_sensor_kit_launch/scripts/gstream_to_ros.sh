dce1f756e25000dd9ff872e76418db1fe23d5d24
dtonda8 <davidbatonda7@gmail.com>
2024-06-23 19:15:31 +1000

    Update camera-feed-receiver launch file
    TODO: somehow launch gstreaming on the px 2 from the alienware

--- twizy_sensor_kit_launch/scripts/gstream_to_ros.sh
+++ twizy_sensor_kit_launch/scripts/gstream_to_ros.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Check if an argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <port>"
+    exit 1
+fi
+
 source install/local_setup.bash && \
-    export GSCAM_CONFIG='udpsrc port=5000 caps="application/x-rtp" ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert' && \
-    ros2 run gscam2 gscam_main --ros-args -p sync_sink:=true -p preroll:=true
+    export GSCAM_CONFIG='udpsrc port='"$1"' caps="application/x-rtp" ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert' && \
+    ros2 run gscam2 gscam_main --ros-args -p sync_sink:=true -p preroll:=true -r /image_raw:=/image0_raw
